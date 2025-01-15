73bcdc65bdf7006b1bf7c2195d9dee757b07ebe5
zzhangje <zhangzrjerry@outlook.com>
2025-01-16 01:09:14 +0800

    refactor

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,16 +1,13 @@
 // pages/login/login.js
 Page({
-  // to_log:{
-  //   window.
-  // },
   /**
    * 页面的初始数据
    */
   data: {
-    alertteam:false,
-    alertpass:false,
-    teamname:0,
-    password:""
+    alertteam: false,
+    alertpass: false,
+    teamname: 0,
+    password: ""
     // "ITOC":{"0":"O","1":"Y","2":"E","3":"X","4":"S","5":"W","6":"L","7":"Q","8":"B","9":"J"},
     // "BBYY":"gdgzez1111",
     // "OOOO":"testonly",
@@ -21,8 +18,8 @@ Page({
    */
   onLoad: function (options) {
     this.setData({
-      teamname:wx.getStorageSync("teamname"),
-      password:wx.getStorageSync('password')
+      teamname: wx.getStorageSync("teamname"),
+      password: wx.getStorageSync('password')
     })
   },
 
@@ -80,49 +77,52 @@ Page({
 
   },
   check_login(event) {
-    console.log(event)          
+    console.log(event)
     wx.setStorage({
-      key:"teamname",
-      data:event.detail.value.teamname
+      key: "teamname",
+      data: event.detail.value.teamname
     })
     wx.setStorage({
-      key:"password",
-      data:event.detail.value.password
+      key: "password",
+      data: event.detail.value.password
     })
     wx.getStorage({
-      key:"teamname",
-      success(res){
-        console.log("getStorage",res)
+      key: "teamname",
+      success(res) {
+        console.log("getStorage", res)
         getApp().globalData.teamname = res.data
       }
     })
     const db = wx.cloud.database()
     wx.cloud.callFunction({
-      name:'LoginCheck',
-      data:{
-        teamname:event.detail.value.teamname,
-        password:event.detail.value.password
+      name: 'LoginCheck',
+      data: {
+        teamname: event.detail.value.teamname,
+        password: event.detail.value.password
       },
-      success:function(res){
+      success: function (res) {
+        // verify the password in the front end
+        // bro can you believe that?
+        // what can I say?
         console.log(res)
-        if(res.result=='success'){
+        if (res.result == 'success') {
           wx.showToast({
             title: '登录成功',
           })
           wx.switchTab({
             url: '../count/count',
           })
-        }else{
+        } else {
           wx.showToast({
             title: '密码错误',
-            icon:'error'
+            icon: 'error'
           })
         }
       },
-      fail:function(res){
+      fail: function (res) {
         wx.showToast({
           title: '查询失败',
-          icon:'error'
+          icon: 'error'
         })
       }
     })
--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -5,9 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // giao:{
-    "比赛类型":"","比赛场次":"","队伍角色":"","队号":"","自动阶段白线":"","自动阶段低球":"","自动阶段高球":"","手动阶段犯规次数":"","手动阶段低球":"","手动阶段高球":"","爬升横档数":"","概述":"","记录员":"","胜负":"","aftertrans_json":"","aftertrans_csv":"","csv_header":"比赛类型,比赛场次,队伍角色,队号,自动阶段白线,自动阶段低球,自动阶段高球,手动阶段航站楼次数,手动阶段低球,手动阶段高球,爬升横档数,概述,记录员",
-  // }
+    "比赛类型": "", "比赛场次": "", "队伍角色": "", "队号": "", "自动阶段白线": "", "自动阶段低球": "", "自动阶段高球": "", "手动阶段犯规次数": "", "手动阶段低球": "", "手动阶段高球": "", "爬升横档数": "", "概述": "", "记录员": "", "胜负": "", "aftertrans_json": "", "aftertrans_csv": "", "csv_header": "比赛类型,比赛场次,队伍角色,队号,自动阶段白线,自动阶段低球,自动阶段高球,手动阶段航站楼次数,手动阶段低球,手动阶段高球,爬升横档数,概述,记录员",
   },
 
   /**
@@ -16,36 +14,35 @@ Page({
   onLoad: function (option) {
     console.log(option)
     this.setData({
-      "比赛类型":option.比赛类型,
-      "比赛场次":option.比赛场次,
-      "队伍角色":option.队伍角色,
-      "队号":option.队号,
-      "自动阶段白线":option.自动阶段白线,
-      "自动阶段低球":option.自动阶段低球,
-      "自动阶段高球":option.自动阶段高球,
-      "手动阶段犯规次数":option.手动阶段犯规次数,
-      "手动阶段低球":option.手动阶段低球,
-      "手动阶段高球":option.手动阶段高球,
-      "爬升横档数":option.爬升横档数,
-      "概述":option.概述,
-      "记录员":option.记录员,
-      "胜负":option.胜负,
+      "比赛类型": option.比赛类型,
+      "比赛场次": option.比赛场次,
+      "队伍角色": option.队伍角色,
+      "队号": option.队号,
+      "自动阶段白线": option.自动阶段白线,
+      "自动阶段低球": option.自动阶段低球,
+      "自动阶段高球": option.自动阶段高球,
+      "手动阶段犯规次数": option.手动阶段犯规次数,
+      "手动阶段低球": option.手动阶段低球,
+      "手动阶段高球": option.手动阶段高球,
+      "爬升横档数": option.爬升横档数,
+      "概述": option.概述,
+      "记录员": option.记录员,
+      "胜负": option.胜负,
     })
     console.log(this.data)
     var trans = "data:{\n"
     var trans_csv = ""
-    var x=["比赛类型","比赛场次","队伍角色","队号","自动阶段白线","自动阶段低球","自动阶段高球","手动阶段犯规次数","手动阶段低球","手动阶段高球","爬升横档数","概述","记录员","胜负"]
-    var y=[this.data.比赛类型,this.data.比赛场次,this.data.队伍角色,this.data.队号,this.data.自动阶段白线,this.data.自动阶段低球,this.data.自动阶段高球,this.data.手动阶段犯规次数,this.data.手动阶段低球,this.data.手动阶段高球,this.data.爬升横档数,this.data.概述,this.data.记录员,this.data.胜负]
-    for(var i=0;i<x.length;i=i+1){
-      // console.log(y[i])
-      trans=trans+"\""+x[i]+"\":"+y[i]+",\n"
-      trans_csv = trans_csv + y[i] +","
+    var x = ["比赛类型", "比赛场次", "队伍角色", "队号", "自动阶段白线", "自动阶段低球", "自动阶段高球", "手动阶段犯规次数", "手动阶段低球", "手动阶段高球", "爬升横档数", "概述", "记录员", "胜负"]
+    var y = [this.data.比赛类型, this.data.比赛场次, this.data.队伍角色, this.data.队号, this.data.自动阶段白线, this.data.自动阶段低球, this.data.自动阶段高球, this.data.手动阶段犯规次数, this.data.手动阶段低球, this.data.手动阶段高球, this.data.爬升横档数, this.data.概述, this.data.记录员, this.data.胜负]
+    for (var i = 0; i < x.length; i = i + 1) {
+      trans = trans + "\"" + x[i] + "\":" + y[i] + ",\n"
+      trans_csv = trans_csv + y[i] + ","
     }
     trans = trans + "}"
     console.log(trans)
     this.setData({
-      aftertrans_json:trans,
-      aftertrans_csv:trans_csv,
+      aftertrans_json: trans,
+      aftertrans_csv: trans_csv,
     })
   },
 
@@ -97,7 +94,7 @@ Page({
   onShareAppMessage: function () {
 
   },
-  copycsvheader:function() {
+  copycsvheader: function () {
     wx.setClipboardData({
       data: this.data.csv_header,
       success: function (res) {
@@ -107,15 +104,15 @@ Page({
               title: '复制成功'
             })
           }
-        
+
+        })
+      }
     })
-  }
-})
   },
-  copycsv:function() {
+  copycsv: function () {
     wx.setClipboardData({
       data: this.data.aftertrans_csv,
-      
+
       success: function (res) {
         wx.getClipboardData({
           success: function (res) {
@@ -123,9 +120,9 @@ Page({
               title: '复制成功'
             })
           }
+        })
+      }
     })
-  }
-})
   },
   copytext: function () {
     // console.log(e)
@@ -142,7 +139,7 @@ Page({
       }
     })
   },
-  tobrowse:function(){
+  tobrowse: function () {
     wx.switchTab({
       url: '../browse/browse',
     })
